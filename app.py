219101f05817a2c90014f7ddc74acf3f1fe467d0
nhan-123 <61376012+nhan-123@users.noreply.github.com>
2022-09-05 13:30:54 +0700

    fix pytest app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
-from flask.logging import create_logger
+import Flask, request, jsonify from flask 
+import create_logger from flask.logging 
 import logging
 
 import pandas as pd
-from sklearn.externals import joblib
+import joblib from sklearn.externals 
 #import joblib
-from sklearn.preprocessing import StandardScaler
+import StandardScaler from sklearn.preprocessing 
 
 app = Flask(__name__)
 LOG = create_logger(app)
